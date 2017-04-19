24d3173d6e29e2181386eba5a9ae47726a8810c9
Bastian Helfert <bastian.helfert@gmail.com>
2017-04-19 19:40:36 +0200

    Add integration tests for success scenarios

--- src/test/java/poc/openshift/greetme/server/api/GreetingsControllerIT.java
+++ src/test/java/poc/openshift/greetme/server/api/GreetingsControllerIT.java
@@ -3,33 +3,86 @@ package poc.openshift.greetme.server.api;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.URI;
+import java.util.Collection;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GreetingsControllerIT {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private TestRestTemplate client;
 
-    private ResponseEntity<String> responseEntity;
+    @LocalServerPort
+    private int port;
 
     @Test
-    public void should_respond_with_not_found_when_non_existent_greeting_is_requested() throws Exception {
+    public void creates_greeting_for_posted_person() throws Exception {
+        // when
+        ResponseEntity<Greeting> response = postPersonToGreet("Luke");
+
+        // then
+        long expectedGreetingId = 1;
+        assertThat(response.getStatusCode()).isEqualTo(CREATED);
+        assertThat(response.getHeaders().getLocation()).isEqualTo(new URI("http://localhost:" + port + "/greetings/" + expectedGreetingId));
+        assertThat(response.getBody()).isEqualTo(new Greeting(expectedGreetingId, "Hello, Luke!"));
+    }
+
+    @Test
+    public void gets_greeting_of_id() throws Exception {
+        // given
+        ResponseEntity<Greeting> responseForPost = postPersonToGreet("Obi Wan");
+        URI location = responseForPost.getHeaders().getLocation();
+
+        // when
+        ResponseEntity<Greeting> response = client.getForEntity(location, Greeting.class);
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(OK);
+        assertThat(response.getBody()).isEqualTo(responseForPost.getBody());
+    }
+
+    @Test
+    public void gets_all_greetings() throws Exception {
+        // given
+        Greeting greeting = postPersonToGreet("Qui-Gon Jinn").getBody();
+
+        // when
+        ResponseEntity<Collection<Greeting>> response = client.exchange("/greetings", HttpMethod.GET, null, new ParameterizedTypeReference<Collection<Greeting>>() {
+        });
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(OK);
+        assertThat(response.getBody()).contains(greeting);
+    }
+
+    @Test
+    public void responds_with_not_found_when_non_existent_greeting_is_requested() throws Exception {
         // given
         String urlOfNonExistentGreeting = "/greetings/9999";
 
         // when
-        responseEntity = restTemplate.getForEntity(urlOfNonExistentGreeting, String.class);
+        ResponseEntity<String> response = client.getForEntity(urlOfNonExistentGreeting, String.class);
 
         // then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-        assertThat(responseEntity.getBody()).isNull();
+        assertThat(response.getStatusCode()).isEqualTo(NOT_FOUND);
+        assertThat(response.getBody()).isNull();
+    }
+
+    private ResponseEntity<Greeting> postPersonToGreet(String personName) {
+        Person person = new Person();
+        person.setName(personName);
+        return client.postForEntity("/greetings", person, Greeting.class);
     }
 }

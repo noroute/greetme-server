8a27376b7727267f09012c6d01c3fc3669d97a70
Bastian Helfert <bastian.helfert@gmail.com>
2017-04-19 09:49:39 +0200

    Remove "should" prefix from test method names

--- src/test/java/poc/openshift/greetme/server/service/GreetingServiceTest.java
+++ src/test/java/poc/openshift/greetme/server/service/GreetingServiceTest.java
@@ -9,18 +9,18 @@ public class GreetingServiceTest {
     private GreetingService greetingService = new GreetingService();
 
     @Test
-    public void should_greet_with_hello_bob_if_name_is_bob() {
+    public void greets_with_hello_bob_if_name_is_bob() {
         String message = greetingService.greetName("Bob");
         assertThat(message).isEqualTo("Hello, Bob!");
     }
 
     @Test(expected = NullPointerException.class)
-    public void should_throw_NullPointerException_if_name_is_null() {
+    public void throws_NullPointerException_if_name_is_null() {
         greetingService.greetName(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void should_throw_IllegalArgumentException_if_name_is_empty() {
+    public void throws_IllegalArgumentException_if_name_is_empty() {
         greetingService.greetName("");
     }
 }

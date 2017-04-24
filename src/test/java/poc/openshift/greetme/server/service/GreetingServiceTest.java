d3046a8e841bd9638cde11ef5edef14ab693461b
Bastian Helfert <bastian.helfert@gmail.com>
2017-04-24 14:25:12 +0200

    Replace redundant testing of preconditions with using PowerMock

--- src/test/java/poc/openshift/greetme/server/service/GreetingServiceTest.java
+++ src/test/java/poc/openshift/greetme/server/service/GreetingServiceTest.java
@@ -1,26 +1,39 @@
 package poc.openshift.greetme.server.service;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import poc.openshift.greetme.server.util.Preconditions;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Preconditions.class})
 public class GreetingServiceTest {
 
     private GreetingService greetingService = new GreetingService();
 
     @Test
+    public void checks_preconditions() throws Exception {
+        // given
+        mockStatic(Preconditions.class);
+
+        // when
+        greetingService.sayHelloTo("Alice");
+
+        // then
+        verifyStatic();
+        Preconditions.checkNotEmpty(eq("Alice"), anyString());
+    }
+
+    @Test
     public void greets_with_hello_bob_if_name_is_bob() {
         String message = greetingService.sayHelloTo("Bob");
         assertThat(message).isEqualTo("Hello, Bob!");
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void throws_NullPointerException_if_name_is_null() {
-        greetingService.sayHelloTo(null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void throws_IllegalArgumentException_if_name_is_empty() {
-        greetingService.sayHelloTo("  ");
     }
 }
